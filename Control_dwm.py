7ca8739736c0d15332038e26ea8bc0844e89fa6b
tongzhangHIT <zhangt_1996@163.com>
2022-06-16 21:57:11 +0800

    delete null log

--- Control_dwm.py
+++ Control_dwm.py
@@ -1,17 +1,17 @@
 from ModuleControlProcess import ProcessCriteria, ControlProcesses
 from time import sleep
-from loguru import logger
-logger.add("log_Control_dwm.txt", format="{time} {level} {message}", level="INFO", encoding="utf-8")
+#from loguru import logger
+#logger.add("log_Control_dwm.txt", format="{time} {level} {message}", level="INFO", encoding="utf-8")
 
 
 if __name__ == "__main__":
-    try:
-        process_for_controlled = ProcessCriteria()
-        process_for_controlled.name = "dwm.exe"
-        process_for_controlled.max_vms_mb = 1024
-        controller = ControlProcesses(process_for_controlled)
-        while True:
-            controller.tick()
-            sleep(600)
-    except Exception as e:
-        logger.exception("")
+#    try:
+    process_for_controlled = ProcessCriteria()
+    process_for_controlled.name = "dwm.exe"
+    process_for_controlled.max_vms_mb = 2048
+    controller = ControlProcesses(process_for_controlled)
+    while True:
+        controller.tick()
+        sleep(600)
+#    except Exception as e:
+#        logger.exception("")
